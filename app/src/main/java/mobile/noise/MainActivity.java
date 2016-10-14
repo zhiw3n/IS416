094408ea5b4a4eb6015b30753d336f84576bd89a
Fabian Schmitt <fabian@schmitt-privat.com>
2016-10-14 20:36:53 +0800

    OpenCV working without memory leakage.

--- app/src/main/java/mobile/noise/MainActivity.java
+++ app/src/main/java/mobile/noise/MainActivity.java
@@ -1,19 +1,25 @@
 package mobile.noise;
 
 import android.app.Activity;
+import android.os.SystemClock;
+import android.provider.Settings;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.WindowManager;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
 
 import static org.opencv.core.Core.countNonZero;
 import static org.opencv.core.CvType.CV_8UC1;
@@ -45,73 +51,65 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback);
 
         setContentView(R.layout.activity_main);
 
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.tutorial1_activity_java_surface_view);
-
-        Log.i(TAG, "onCreate() " + mOpenCvCameraView.toString());
-
+        mOpenCvCameraView.setMaxFrameSize(400, 400);
+        mOpenCvCameraView.setCameraIndex(CameraBridgeViewBase.CAMERA_ID_FRONT);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
+    }
 
-        m1 = null;
-        m2 = null;
-        m3 = null;
-        diff1 = null;
-        diff2 = null;
-        result = null;
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
     }
 
     @Override
     public void onCameraViewStarted(int width, int height) {
+        diff1 = new Mat(height, width, CvType.CV_8UC1);
+        diff2 = new Mat(height, width, CvType.CV_8UC1);
+        result = new Mat(height, width, CvType.CV_8UC1);
 
+        mats = new ArrayList<Mat>();
     }
 
     @Override
     public void onCameraViewStopped() {
-
+        diff1.release();
+        diff2.release();
+        result.release();
     }
 
-    private Mat m1, m2, m3, diff1, diff2, result, tmp;
+    private Mat diff1, diff2, result;
+    private ArrayList<Mat> mats;
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        if (m1 == null) {
-            m1 = inputFrame.rgba();
-            tmp = new Mat(m1.size(), CV_8UC1);
-            Imgproc.cvtColor(m1, tmp, Imgproc.COLOR_RGBA2GRAY);
-            m1 = tmp;
-        } else if (m2 == null) {
-            m2 = inputFrame.rgba();
-            tmp = new Mat(m2.size(), CV_8UC1);
-            Imgproc.cvtColor(m2, tmp, Imgproc.COLOR_RGBA2GRAY);
-            m2 = tmp;
-        } else if (m3 == null) {
-            m3 = inputFrame.rgba();
-            tmp = new Mat(m3.size(), CV_8UC1);
-            Imgproc.cvtColor(m3, tmp, Imgproc.COLOR_RGBA2GRAY);
-            m3 = tmp;
+        SystemClock.sleep(200);
 
-            diff1 = new Mat(m1.size(), m1.depth());
-            Core.absdiff(m1, m2, diff1);
+        if (mats.size() < 3) {
+            mats.add(inputFrame.gray());
+        } else {
+            mats.add(inputFrame.gray());
 
-            diff2 = new Mat(m1.size(), m1.depth());
-            Core.absdiff(m2, m3, diff2);
-
-            result = new Mat(m1.size(), m1.depth());
+            Core.absdiff(mats.get(0), mats.get(1), diff1);
+            Core.absdiff(mats.get(1), mats.get(2), diff2);
             Core.bitwise_and(diff1, diff2, result);
+
+            Imgproc.threshold(result, result, 40, 255, Imgproc.THRESH_TOZERO);
 
             if (countNonZero(result) > 0) {
                 Log.i(TAG, "There was movement with " + countNonZero(result) + " elements.");
             }
 
-            m1 = m2;
-            m2 = m3;
-            m3 = null;
-
+            mats.get(0).release();
+            mats.remove(0);
             return result;
         }
 
