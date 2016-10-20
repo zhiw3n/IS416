8b6c2d1964aa3e8df6c61bab87535f13b9276206
Fabian Schmitt <fabian@schmitt-privat.com>
2016-10-20 15:34:33 +0800

    Other half of the files: UI mostly implemented. Sensor services are started & stopped on button click. Settings are remembered. Switch between activities possible. Fallback for Android below API 14 is implemented (still needs to be tested). However, settings can still be changed even if services are running.

---
+++ app/src/main/java/mobile/noise/FloorActivity.java
@@ -0,0 +1,15 @@
+package mobile.noise;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FloorActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floor);
+    }
+}
