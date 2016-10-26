beee1bb50eda523e951b1f815c4a73dbffd5fa28
stellareddy <s.tellaaa@hotmail.com>
2016-10-26 10:44:37 +0800

    Changes made to Spinner, realize was not working

--- app/src/main/java/mobile/noise/SettingsActivity.java
+++ app/src/main/java/mobile/noise/SettingsActivity.java
@@ -3,7 +3,9 @@ package mobile.noise;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
+import android.widget.Spinner;
 
 import mobile.noise.mobile.noise.sensorservices.SensorType;
 
@@ -12,19 +14,19 @@ import mobile.noise.mobile.noise.sensorservices.SensorType;
 
 public class SettingsActivity extends Activity {
 
-    String [] SPINNERLIST = {"1 sec","10 secs","20 secs","30 secs"};
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        /*
-        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,
-                android.R.layout.simple_dropdown_item_1line, SPINNERLIST);
-        MaterialBetterSpinner betterSpinner=(MaterialBetterSpinner) findViewById(R.id.duration_dropdown);
-        betterSpinner.setAdapter(arrayAdapter);
-        */
+        Spinner spinner = (Spinner) findViewById(R.id.durationSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+        R.array.duration_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
 
         findViewById(R.id.accelerometerSwitch).setOnClickListener(new View.OnClickListener() {
             @Override
