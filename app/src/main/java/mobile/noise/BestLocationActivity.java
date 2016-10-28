78a74c86d04218c274990265dcf0c1fe82d537c3
Fabian Schmitt <fabian@schmitt-privat.com>
2016-10-28 10:05:34 +0800

    Best location layout by Angelia added.

---
+++ app/src/main/java/mobile/noise/BestLocationActivity.java
@@ -0,0 +1,26 @@
+package mobile.noise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class BestLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_best_location);
+    }
+
+    public void firstPlaceClicked(View v) {
+        // TODO Logic
+    }
+
+    public void secondPlaceClicked(View v) {
+        // TODO Logic
+    }
+
+    public void thirdPlaceClicked(View v) {
+        // TODO Logic
+    }
+}
